86d49cba393a720de38d10efb9578bc7f1656ce9
Kone <117305356+PhilipKone@users.noreply.github.com>
2026-01-01 12:06:06 +0000

    Revert "Save course changes"
    This reverts commit 25c98dc59152c6f58f1db91eff16768596b22785.

--- src/courses/intro-python/compare.py
+++
@@ -1,9 +0,0 @@
-x = int(input("Enter an integer: "))
-y = int(input("Enter another integer: "))
-
-if x > y:
-    print(f"{x} is greater than {y}")
-elif x < y:
-    print(f"{x} is less than {y}")
-else:
-    print(f"{x} is equal to {y}")
--- src/courses/intro-python/hello.py
+++ src/courses/intro-python/hello.py
@@ -1,16 +1,6 @@
 # This is a simple Python program that greets the user by name
 # It prompts the user to enter their name
-name = input("what's your name? ").strip().title()
+name = input("what's your name? ")
 
 # Greet the user with their name
 print(f"Hello, {name}!")
-
-
-
-# End of the program
-def main():
-    name = input("what's your name? ").strip().title() 
-    hello(name)
-
-def hello(name = "world"):
-    print(f"Hello, {name}!")    
