00e8355d935c378ef7f441f11392fe1fe5b43836
Kone <117305356+PhilipKone@users.noreply.github.com>
2026-01-01 12:06:06 +0000

    Revert "Add intro-python course content"
    This reverts commit a464a4c084ca1da278e6872bb22fe8c5aa5ed995.

--- src/courses/intro-python/hello.py
+++
@@ -1,6 +0,0 @@
-# This is a simple Python program that greets the user by name
-# It prompts the user to enter their name
-name = input("what's your name? ")
-
-# Greet the user with their name
-print(f"Hello, {name}!")
