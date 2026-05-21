880fa7cfbdf21e192122d1247153befdc96b69bb
Kone <philipkone45@.com>
2026-05-21 17:39:13 +0000

    feat: implement react-snap for SSG to resolve Search Console 404 indexing errors

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,32 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'
+import { hydrateRoot, createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 import ErrorBoundary from './components/ErrorBoundary'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <ErrorBoundary appName="Kone-Code">
-            <HashRouter>
-                <App />
-            </HashRouter>
-        </ErrorBoundary>
-    </React.StrictMode>,
-)
+const rootElement = document.getElementById('root');
+
+if (rootElement.hasChildNodes()) {
+    hydrateRoot(
+        rootElement,
+        <React.StrictMode>
+            <ErrorBoundary appName="Kone-Code">
+                <BrowserRouter>
+                    <App />
+                </BrowserRouter>
+            </ErrorBoundary>
+        </React.StrictMode>
+    );
+} else {
+    const root = createRoot(rootElement);
+    root.render(
+        <React.StrictMode>
+            <ErrorBoundary appName="Kone-Code">
+                <BrowserRouter>
+                    <App />
+                </BrowserRouter>
+            </ErrorBoundary>
+        </React.StrictMode>
+    );
+}
